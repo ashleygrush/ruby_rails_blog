b2c83e30a99717479a2347f6d7c39a23add0e135
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-10-05 14:10:54 -0700

    added delete function; CRUD complete.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,24 @@
 class ArticlesController < ApplicationController
+
    def index
     @articles = Article.all
   end
+
 
    def show
      @article = Article.find(params[:id])
   end
 
+
   def new
     @article = Article.new   
   end
 
+
   def edit
      @article = Article.find(params[:id])
   end
+
 
   def create
      @article = Article.new(article_params)
@@ -25,6 +30,7 @@ class ArticlesController < ApplicationController
      end
   end
 
+
   def update
      @article = Article.find(params[:id])
  
@@ -34,7 +40,16 @@ class ArticlesController < ApplicationController
        render 'edit'
      end
   end
+
+
+  def destroy
+     @article = Article.find(params[:id])
+     @article.destroy
  
+     redirect_to articles_path
+  end
+
+
   private
   def article_params
      params.require(:article).permit(:title, :text)
